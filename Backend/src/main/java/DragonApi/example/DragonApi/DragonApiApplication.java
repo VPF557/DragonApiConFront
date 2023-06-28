4d28dd0b76387975f00e404c78124ba96ed6f5c3
VPF557 <pedrojosepaniagua01@gmail.com>
2023-06-28 17:24:25 +0200

    Añadida dependendia al POM

---
+++ Backend/src/main/java/DragonApi/example/DragonApi/DragonApiApplication.java
@@ -0,0 +1,13 @@
+package DragonApi.example.DragonApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonApiApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/DragonApi/example/DragonApi/DragonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package DragonApi.example.DragonApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
