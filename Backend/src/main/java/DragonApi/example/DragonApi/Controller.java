c492fa2b593f424d7acffba569ef174d49ac393a
VPF557 <pedrojosepaniagua01@gmail.com>
2023-06-28 17:52:28 +0200

    Refactorizado peticiones

--- Backend/src/main/java/DragonApi/example/DragonApi/Controller.java
+++ Backend/src/main/java/DragonApi/example/DragonApi/Controller.java
@@ -13,7 +13,7 @@ public class Controller {
     LeerJson reader = new LeerJson();
     DataHanding data = new DataHanding();
     EscribirJson writter = new EscribirJson();
-
+/*
     @GetMapping("/registro")
     public ArrayList<Parametro> listaObjeto1()
     {
@@ -22,34 +22,33 @@ public class Controller {
         //Mostramos los elementos leidos
         return listaPeticiones;
     }
-
+*/
     @PostMapping("/peticion")
     public void mandarElemento(@RequestBody Parametro objeto1)
     {
         //Hecho
-        ArrayList<Pokemon> listaPokemons = reader.LeerFicheroPokemons("Pokemons.json");
+        ArrayList<Personaje> listaPersonaje = reader.LeerFicheroPersonajes("Personajes.json");
         ArrayList<Parametro> listaPeticiones = reader.LeerFicheroPeticiones("Peticiones.json");
-        ArrayList<Tipo> listaTipo = reader.LeerFicheroTipos("Tipos.json");
+        ArrayList<Fusion> listaFusion = reader.LeerFicheroFusion("Fusiones.json");
 
         if(objeto1.getParameter1().equals("type"))
         {
-            Tipo tipo = new Tipo();
-            tipo = data.obtenerTipo(objeto1);
-            listaTipo.add(tipo);
-            writter.escribirJsonTipo(listaTipo);
+            Fusion fusion = new Fusion();
+            fusion = data.obtenerFusion(objeto1);
+            listaFusion.add(fusion);
+            writter.escribirJsonFusion(listaFusion);
 
         }
         else
         {
-            Pokemon pokemon = new Pokemon();
-            pokemon = data.obtenerPokemon(objeto1);
-            listaPokemons.add(pokemon);
-            writter.escribirJsonPokemon(listaPokemons);
+            Personaje personaje = new Personaje();
+            personaje = data.obtenerPersonaje(objeto1);
+            listaPersonaje.add(personaje);
+            writter.escribirJsonPersonaje(listaPersonaje);
 
         }
 
         listaPeticiones.add(objeto1);
-
         writter.escribirJsonPeticiones(listaPeticiones);
     }
 
---
+++ Backend/src/main/java/DragonApi/example/DragonApi/DataHanding.java
@@ -0,0 +1,88 @@
+package DragonApi.example.DragonApi;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class DataHanding {
+
+    private final static String url = "https://dragonballapi-backend.vercel.app/%s/information/%s";
+
+    public Personaje obtenerPersonaje(Parametro params)
+    {
+
+        Personaje personaje = null;
+
+        String peopleUrl = String.format(url, params.getParameter1(), params.getParameter2());
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(peopleUrl))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = HttpClient
+                    .newBuilder()
+                    .build()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String jsonData = response.body();
+
+            Gson gson = new Gson();
+            personaje = gson.fromJson(jsonData, Personaje.class);
+
+        } catch (URISyntaxException e) {
+            System.out.println("Error al crear la request: " + e.getMessage());
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return personaje;
+    }
+
+    public Fusion obtenerFusion(Parametro params)
+    {
+
+        Fusion fusion = null;
+
+        String peopleUrl = String.format(url, params.getParameter1(), params.getParameter2());
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(peopleUrl))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = HttpClient
+                    .newBuilder()
+                    .build()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String jsonData = response.body();
+
+            Gson gson = new Gson();
+            tipo = gson.fromJson(jsonData, Fusion.class);
+
+        } catch (URISyntaxException e) {
+            System.out.println("Error al crear la request: " + e.getMessage());
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return fusion;
+    }
+
+}
